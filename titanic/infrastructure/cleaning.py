fa225d45e932c2c41fe9f9e4271693b60cf8aca7
carmenBtl <prudencia.bataille@gmail.com>
2023-12-19 16:47:29 +0100

    cleaningPart

--- titanic/infrastructure/cleaning.py
+++ titanic/infrastructure/cleaning.py
@@ -2,24 +2,85 @@ import numpy as np
 import pandas as pd
 
 
-def clean(df: pd.DataFrame) -> pd.DataFrame:
+def goodColumnsFormat(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Clean a raw extract of data.
+    This function rename the columns of a DataFrame.
 
-    Parameters
-    ----------
-    df : pd.DataFrame
-        Input data to clean.
+    Parameters :
+    - dataframe : dataframe that need to be updated with the new names of columns.
+    - nouveaux_noms : Une liste de nouveaux noms de colonnes.
 
-    Returns
-    -------
-    pd.DataFrame
-        Cleaned data.
+    Return :
+    - A new DataFrame with the renamed colums.
     """
+    df['pclass'] = pd.to_numeric(df['pclass'])
+    df['survival'] = pd.to_numeric(df['survival'])
     df['age'] = pd.to_numeric(df['age'])
     df['fare'] = pd.to_numeric(df['fare'])
+    df['sibsp'] = pd.to_numeric(df['sibsp'])
+    df['parch'] = pd.to_numeric(df['parch'])
 
-
-    df = df.drop_duplicates()
-    df = df.dropna()
     return df
+
+
+def renameColumns(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    This function rename the columns of a DataFrame.
+
+    Parameters :
+    - dataframe : dataframe that need to be updated with the new names of columns.
+    - nouveaux_noms : Une liste de nouveaux noms de colonnes.
+
+    Return :
+    - A new DataFrame with the renamed colums.
+    """
+
+    df = df.rename(columns={"pclass": "passengerClass",
+                         "sibsp": "siblingsOrSpouses",
+                         "parch": "parentsOrChildren",
+                         "home.dest" : "homeDestination"
+                         })
+    return df   
+
+
+
+def replacingNA(df: pd.DataFrame) -> pd.DataFrame:
+    """  
+    This function remove the missing values.
+
+    Parameters :
+    - dataframe : dataframe that need to be updated with the missings values. 
+    Return :
+    - A new DataFrame with the missings values replaced         .
+    """
+    df = df.fillna('NA')
+    return df
+
+
+
+def removeDuplicates(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    This function remove the duplicates rows of the DataFrame.
+
+    Parameters :
+    - dataframe : dataframe that need to be updated with the new names of columns.
+
+    Return :
+    - A new DataFrame with no duplicated rows.
+    """
+    df = df.drop_duplicates()
+    return df    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
