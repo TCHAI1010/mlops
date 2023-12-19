2395d8cc7e349ce2b35faa22a23421f08178f06d
qiuqiuuuu <quynhtrinh147@gmail.com>
2023-12-19 16:42:08 +0100

    ok

---
+++ titanic/infrastructure/quanti_stat_des.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_histogram (data: pd.DataFrame):
+    """
+    Plot histograms for quantitative variables in the dataset.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Input data to generate plots.
+
+    """
+    for i in ['age', 'siblingsOrSpouses','parentsOrChildren', 'fare']:
+
+        filtered_data=data[i][data[i] != 'NA']
+        
+        plt.hist(filtered_data, bins=30, color='skyblue', edgecolor='black')
+
+        plt.title(f'Répartition de {i}')
+        plt.xlabel('Valeurs')
+        plt.ylabel('Fréquence')
+        
+        plt.show()
