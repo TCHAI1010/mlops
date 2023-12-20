a0cbc5340b7bba7a146445c6741b4e1823c24402
qiuqiuuuu <quynhtrinh147@gmail.com>
2023-12-20 13:42:44 +0100

    ok

--- titanic/domain/feature_engineering.py
+++ titanic/domain/feature_engineering.py
@@ -9,7 +9,7 @@ def transfomm_homedest(data:pd.DataFrame)-> pd.DataFrame:
 
     Parameters
     ----------
-    df : pd.DataFrame
+    data : pd.DataFrame
         Input dataframe. Should have 'homeDestination'.
 
     Returns
@@ -38,7 +38,7 @@ def transfomm_homedest(data:pd.DataFrame)-> pd.DataFrame:
 
 
 
-def feature_selection(data:pd.DataFrame, selected_colums:list)-> pd.DataFrame:
+def feature_selection(data:pd.DataFrame, selected_columns:list)-> pd.DataFrame:
     """
     Get the right dataframe for prediction.
 
@@ -59,7 +59,7 @@ def feature_selection(data:pd.DataFrame, selected_colums:list)-> pd.DataFrame:
     return(df)
 
 
-def name(data:pd.DataFrame)-> pd.DataFrame:
+def encode_features(data:pd.DataFrame)-> pd.DataFrame:
     """
     Create age and fare classes. Shoulde have 'age' and 'fare'
 
@@ -73,5 +73,48 @@ def name(data:pd.DataFrame)-> pd.DataFrame:
     pd.DataFrame
         output dataframe age and fare class.
     """ 
-    
+
+    df=data.copy()
+
+    age_class = [0, 18, 26, 41, 56, 100]
+    label_age = ['0-17', '18-25', '26-40', '41-55', '55+']
+    fare_class = [0, 26, 51, 600]
+    label_fare = ['0-25', '26-50', '50+']
+
+    df['age'].replace('NA', pd.NA, inplace=True)
+    df['fare'].replace('NA', pd.NA, inplace=True)
+
+    df.loc[df['age'].notna(), 'age'] = pd.cut(df['age'][df['age'].notna()], bins=age_class, labels=label_age, right=False)
+
+    df.loc[df['fare'].notna(), 'fare'] = pd.cut(df['fare'][df['fare'].notna()], bins=fare_class, labels=label_fare, right=False)
+
+
+    df['age'].replace(pd.NA,'NA',  inplace=True)
+    df['fare'].replace( pd.NA,'NA', inplace=True)
+
+    return(df)
+
+
+def getDummies(data: pd.DataFrame, cols: list)-> pd.DataFrame:
+    """
+    Create dummy variables.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Input dataframe.
+
+    Returns
+    -------
+    pd.DataFrame
+        output dataframe with dummy variables.
+    """ 
+
+    df=pd.get_dummies(data, columns=cols, drop_first=True,dtype=float)
+    return(df)
+
+
+
+
+
 
